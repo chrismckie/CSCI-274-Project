4a321dd18d98da9b8db3a8c8c89e52b34862bc3a
chrismckie <chrismckie19@mail.fresnostate.edu>
2025-12-06 22:28:19 -0800

    run algorithm

--- python/data_processing.py
+++ python/data_processing.py
@@ -14,22 +14,28 @@ def get_week_by_start_date(df, week_start):
 
     # Determine column names based on dataframe input
     if df.columns[0] == "appointment_id":
+        is_medical_data = True
+
         # Process medical data
         id = "appointment_id"
-        job_date = "appointment_date"
-        start = "start_minutes"
-        end = "end_minutes"
-        priority = "priority"
+        job_date = "appointment_date"      
     else:
+        is_medical_data = False
+
         # Process cloud data
         id = "job_interval"
         job_date = "job_date"
-        start = "start_minutes"
-        end = "end_minutes"
-        priority = "priority"
+  
+    # These values are the same across both datasets
+    start_time = 'start_time'
+    end_time = 'end_time'
+    start = "start_minutes"
+    end = "end_minutes"
+    priority = "priority"
 
+    num_weeks = 3 if is_medical_data else 1     # Medical data combines appointments over multiple weeks
     week_start = pd.to_datetime(week_start)
-    week_end = week_start + pd.Timedelta(days=7)
+    week_end = week_start + pd.Timedelta(days=7 * num_weeks)
 
     df = df.copy()
     df[job_date] = pd.to_datetime(df[job_date])
@@ -40,15 +46,28 @@ def get_week_by_start_date(df, week_start):
 
     # Group dataframes by day of the week
     day_frames = {}
-    for date, group in week_df.groupby(week_df[job_date].dt.date):
-        day_name = pd.to_datetime(date).strftime('%A')
 
-        # Sort by start time
-        day_frames[day_name] = (
-            group[[id, start, end, priority]]
-            .sort_values(start)
-            .reset_index(drop=True)
-        )
+    if is_medical_data:
+        # Medical data groups 3 weeks of data by weekday
+        week_df["weekday"] = week_df[job_date].dt.day_name()
+
+        for day_name, group in week_df.groupby("weekday"):
+            day_frames[day_name] = (
+                group[[id, start_time, end_time, start, end, priority]]
+                .sort_values(start)
+                .reset_index(drop=True)
+            )
+    else:
+        # Cloud data groups 1 week of data by weekday
+        for date, group in week_df.groupby(week_df[job_date].dt.date):
+            day_name = pd.to_datetime(date).strftime('%A')
+
+            # Sort by start time
+            day_frames[day_name] = (
+                group[[id, start_time, end_time, start, end, priority]]
+                .sort_values(start)
+                .reset_index(drop=True)
+            )
     return day_frames
 
 # Returns data frame w columns: appointment_id, start_time, end_time, priority
@@ -64,7 +83,13 @@ def load_medical_data(filepath):
     # Prioritizes appointments by the age of patient
     df['priority'] = df['age']
 
-    result = df[['appointment_id', 'appointment_date', 'start_minutes', 'end_minutes', 'priority']]
+    result = df[['appointment_id', 
+                 'appointment_date', 
+                 'start_time', 
+                 'end_time', 
+                 'start_minutes', 
+                 'end_minutes', 
+                 'priority']]
 
     return result
 
@@ -83,7 +108,13 @@ def load_cloud_data(filepath):
     priority_map = {'low': 1, 'medium': 2, 'high': 3}
     df['priority'] = df['Priority_Level'].str.lower().map(priority_map)
 
-    result = df[['Job_ID', 'job_date', 'start_minutes', 'end_minutes', 'priority']]
-    result = result.rename(columns={'Job_ID': 'job_interval'})
+    result = df[['Job_ID', 
+                 'job_date', 
+                 'Start_Time', 
+                 'End_Time', 
+                 'start_minutes', 
+                 'end_minutes', 
+                 'priority']]
+    result = result.rename(columns={'Job_ID': 'job_interval', 'Start_Time': 'start_time', 'End_Time': 'end_time'})
 
     return result
